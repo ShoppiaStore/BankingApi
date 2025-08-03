7ad181dac2e36d47d246d95cd01bd0060ee944e1
Taghreed Ali <taghreed1225@gmail.com>
2025-08-03 15:37:05 +0300

    add to shop

--- src/main/java/com/progect/BankingApp/controller/ValidationController.java
+++ src/main/java/com/progect/BankingApp/controller/ValidationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:4300"})
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("bank/token/validation")
 public class ValidationController {
